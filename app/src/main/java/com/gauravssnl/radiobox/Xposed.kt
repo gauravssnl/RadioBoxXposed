af10934bfee10d36e65341db64752dca9d1080ab
Gaurav <gaurav.ssnl@gmail.com>
2024-10-13 16:06:04 +0530

    Add method filtering code for future usage. It would help us if the method name changes in future.

--- app/src/main/java/com/gauravssnl/radiobox/Xposed.kt
+++ app/src/main/java/com/gauravssnl/radiobox/Xposed.kt
@@ -5,6 +5,9 @@ import de.robv.android.xposed.XC_MethodReplacement
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import java.lang.reflect.Method
+import java.lang.reflect.Modifier
+import java.util.Objects
 
 class Xposed : IXposedHookLoadPackage {
 
@@ -16,7 +19,20 @@ class Xposed : IXposedHookLoadPackage {
             XposedHelpers.findClassIfExists("$packageName.MainActivity", lpParam.classLoader)
         val hashMap = hashMapOf<String, Any>("noAd" to true)
         val methodName = "L0"
-        XposedHelpers.findAndHookMethod(clazz, methodName, XC_MethodReplacement.returnConstant(hashMap))
+        // Example Method signature :  public final HashMap<String, Object> m()
+        val methods = clazz.declaredMethods
+        var filteredMethods = methods.filter {
+            it.parameterCount == 0 && Modifier.isPublic(
+                it.modifiers
+            ) && Modifier.isFinal(it.modifiers)
+                    && it.returnType == HashMap::class.java
+        }
+        filteredMethods = filteredMethods.filter { methodName == it.name }
+        if (filteredMethods.size == 1) {
+            val method = filteredMethods[0]
+            XposedBridge.log("Found the method for hooking :: $method")
+            XposedBridge.hookMethod(method, XC_MethodReplacement.returnConstant(hashMap))
+        }
         XposedBridge.log("All hooking completed for the package $packageName")
     }
 
